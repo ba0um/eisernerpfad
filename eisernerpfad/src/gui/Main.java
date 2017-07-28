aff6336ca3e1d032e4d0a8fee522591576703f40
Kim <kim.dienelt@gmail.com>
2017-07-28 13:41:55 +0200

    Changed Main slightly (about to merge with local repo).

--- eisernerpfad/src/gui/Main.java
+++ eisernerpfad/src/gui/Main.java
@@ -5,7 +5,6 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
 
